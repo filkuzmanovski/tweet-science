0b7de829782a4ffeef608806078fc474acac9b2e
Filip Kuzmanovski <filkuzmanovski@gmail.com>
2016-12-17 22:52:02 -0800

    File Structure Changes
    All files using matching syntax

--- twitter_get_home_timeline.py
+++ twitter_get_home_timeline.py
@@ -5,8 +5,8 @@ from twitter_client import get_twitter_client
 if __name__ == '__main__':
     client = get_twitter_client()
 
-    with open('home_timeline.jsonl', 'w') as f:
-        for page in Cursor(client.home_timeline, count=200, include_rts=True).pages(4):
+    with open('home_timeline.jsonl', 'w') as f: # Output in JSON Lines format
+        for page in Cursor(client.home_timeline, count=200, include_rts=True).pages(4): # 200 Tweets, 4 Pages
             for status in page:
                 # Process a status
                 f.write(json.dumps(status._json)+ "\n")
